3a0a1eee26c96ba93449c342595749eeb0542692
Ernesto <jorge.cywdt@gmail.com>
2021-11-15 19:40:09 -0500

    Maquetación de vistas en Bootstrap

---
+++ views/calling_bingo_numbers.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bingo Kata App</title>
+    <?php include 'layout/head.php'; ?>
+</head>
+<body>
+    <?php include 'layout/header.php'; ?>
+
+    <!-- CONTAINER -->
+    <div class="container">
+        <div class="row justify-content-center mt-3 pt-2">
+            <div class="col-md-12">
+
+                <?php include 'layout/footer.php'; ?>
+
+            </div>
+        </div>
+    </div>
+    <!-- END CONTAINER -->
+
+    <?php include 'layout/scripts.php'; ?>
+</body>
+</html>
